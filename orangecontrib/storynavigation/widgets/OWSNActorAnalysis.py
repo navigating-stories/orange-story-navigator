bde8a181cd13e48399e5f038d8302a334ab22324
Flavio Hafner <f.hafner@esciencecenter.nl>
2024-01-04 13:48:39 +0100

    do not download spacy model for each run

--- orangecontrib/storynavigation/widgets/OWSNActorAnalysis.py
+++ orangecontrib/storynavigation/widgets/OWSNActorAnalysis.py
@@ -50,7 +50,7 @@ from orangecontrib.text.corpus import Corpus
 from storynavigation.modules.actoranalysis import ActorTagger
 import storynavigation.modules.constants as constants
 
-spacy.cli.download(constants.NL_SPACY_MODEL)
+# spacy.cli.download(constants.NL_SPACY_MODEL)
 
 HTML = """
 <!doctype html>
